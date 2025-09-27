08cdb86756248ba5de621a7ebc588df98cd24cdc
blueskybone <702133105@qq.com>
2025-09-27 18:21:24 +0800

    寻访统计界面新增两个fragment用于展示统计数据和原始数据;修复了GachaModel在十连中的错误，和根据卡池名称错误合并标准池

--- app/src/main/java/com/blueskybone/arkscreen/ui/fragment/GachaText.kt
+++ app/src/main/java/com/blueskybone/arkscreen/ui/fragment/GachaText.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.blueskybone.arkscreen.databinding.FragmentGachaTextBinding
 import com.blueskybone.arkscreen.ui.recyclerview.GachaTextAdapter
 import com.blueskybone.arkscreen.viewmodel.GachaModel
 
-class GachaText: Fragment() {
+class GachaText : Fragment() {
     private val model: GachaModel by viewModels()
-    private var adapter: GachaTextAdapter? = null
+
+    private lateinit var adapter: GachaTextAdapter
 
     private var _binding: FragmentGachaTextBinding? = null
     private val binding get() = _binding!!
@@ -20,20 +22,35 @@ class GachaText: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
+
+        adapter = GachaTextAdapter(requireContext(), 100)
         _binding = FragmentGachaTextBinding.inflate(inflater)
         setupBinding()
         setUpObserver()
+        setupListener()
         return binding.root
     }
 
     private fun setupBinding() {
-        adapter = GachaTextAdapter(requireContext())
         binding.RecyclerView.adapter = adapter
     }
 
     private fun setUpObserver() {
-        model.gachaRecords.observe(requireActivity()) { value ->
-            adapter?.submitList(value)
+        model.gachaRecordsCount.observe(requireActivity()) { value ->
+            adapter.refreshData(value)
+            binding.RecyclerView.scrollToPosition(0)
         }
+    }
+
+    private fun setupListener() {
+        val rv = binding.RecyclerView
+        rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(1)) {
+                    recyclerView.post { adapter.loadMoreData() }
+                }
+            }
+        })
     }
 }
