331bb564c687534104c1ec345c9f0140b22c3229
blueskybone <702133105@qq.com>
2025-12-26 00:07:14 +0800

    添加卡池类型

--- app/src/main/java/com/blueskybone/arkscreen/network/NetWorkTask.kt
+++ app/src/main/java/com/blueskybone/arkscreen/network/NetWorkTask.kt
@@ -13,6 +13,7 @@ import com.blueskybone.arkscreen.network.model.PlayerInfoResp
 import com.blueskybone.arkscreen.room.AccountGc
 import com.blueskybone.arkscreen.room.AccountSk
 import com.blueskybone.arkscreen.room.Gacha
+import com.blueskybone.arkscreen.util.toCate
 import retrofit2.Response
 
 /**
@@ -105,13 +106,6 @@ class NetWorkTask {
                 }
             }
             return records.toList()
-        }
-
-        private fun String.toCate(): String {
-            if (this.startsWith("LIMITED") || this.startsWith("LINKAGE")) return "LIMITED"
-            if (this.startsWith("CLASSIC")|| this.startsWith("FESCLASSIC") ) return "CLASSIC"
-            if (this.startsWith("SINGLE") || this.startsWith("DOUBLE") || this.startsWith("SPECIAL") || this.startsWith("NORM")) return "NORMAL"
-            return "UN"
         }
     }
 
